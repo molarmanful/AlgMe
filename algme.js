5abad37a77d27df0b752a4a39cbea7042075f787
molarmanful <molarmanful@users.noreply.github.com>
2015-03-06 17:39:06 -0800

    Update algme.js

--- algme.js
+++ algme.js
@@ -1,22 +1,22 @@
-//get jQuery
-if(typeof jQuery == 'undefined' || jQuery().jquery != '2.1.3'){
-    var jq = document.createElement('script');
-    jq.src = "https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js";
-    document.head.appendChild(jq);
-}
-
-//preliminary functions
-function getSelectionText() {
-    var text = "";
-    if (window.getSelection) {
-        text = window.getSelection().toString();
-    } else if (document.selection && document.selection.type != "Control") {
-        text = document.selection.createRange().text;
-    }
-    return text;
-}
-
 (function(){
+  //get jQuery
+    if(typeof jQuery == 'undefined' || jQuery().jquery != '2.1.3'){
+        var jq = document.createElement('script');
+        jq.src = "https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js";
+        document.head.appendChild(jq);
+    }
+    
+    //preliminary functions
+    function getSelectionText() {
+        var text = "";
+        if (window.getSelection) {
+            text = window.getSelection().toString();
+        } else if (document.selection && document.selection.type != "Control") {
+            text = document.selection.createRange().text;
+        }
+        return text;
+    }
+
   //get cube visualizer script
   $.getScript('http://molarmanful.github.io/gCube/gcube.js').done(function(){
     $('body').children().select(function(){
