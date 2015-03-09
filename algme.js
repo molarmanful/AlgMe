e709bbe0c66baaa9f41ec6b93e1820c8715de5a0
molarmanful <molarmanful@users.noreply.github.com>
2015-03-09 11:37:25 -0700

    Update algme.js

--- algme.js
+++ algme.js
@@ -1,42 +1,40 @@
 (function(){
   //get jQuery
-    if(typeof jQuery == 'undefined'){
-        var jq = document.createElement('script');
-        jq.src = "https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js";
-        document.head.appendChild(jq);
+  var jq = document.createElement('script');
+  jq.src = "https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js";
+  document.head.appendChild(jq);
+  
+  //preliminary functions
+  function getSelectionText() {
+    var text = "";
+    if (window.getSelection) {
+      text = window.getSelection().toString();
+    } else if (document.selection && document.selection.type != "Control") {
+      text = document.selection.createRange().text;
     }
-    
-    //preliminary functions
-    function getSelectionText() {
-        var text = "";
-        if (window.getSelection) {
-            text = window.getSelection().toString();
-        } else if (document.selection && document.selection.type != "Control") {
-            text = document.selection.createRange().text;
-        }
-        return text;
-    }
+    return text;
+  }
 
   //get cube visualizer script
   $.getScript('http://molarmanful.github.io/gCube/gcube.js').done(function(){
     $('body').children().select(function(){
-        var alg = getSelectionText();
-        if(alg != ''){
-            if($('#gcubeviz').length){
-                $('#gcubeviz').setalg(alg);
-            } else {
-                $('body').append('<g-cube id="gcubeviz"><g-algorithm>' + alg + '</g-algorithm></g-cube>');
-                $('#gcubeviz').css({
-                    'position': 'fixed',
-                    'height': '10%',
-                    'width': '10%',
-                    'top': '5%',
-                    'right': '5%',
-                    'background-color': '#ffffff',
-                    'z-index': '1000'
-                });
-            }
+      var alg = getSelectionText();
+      if(alg != ''){
+        if($('#gcubeviz').length){
+          $('#gcubeviz').setalg(alg);
+        } else {
+          $('body').append('<g-cube id="gcubeviz"><g-algorithm>' + alg + '</g-algorithm></g-cube>');
+          $('#gcubeviz').css({
+              'position': 'fixed',
+              'height': '10%',
+              'width': '10%',
+              'top': '5%',
+              'right': '5%',
+              'background-color': '#ffffff',
+              'z-index': '1000'
+          });
         }
+      }
     });
   });
 })();
